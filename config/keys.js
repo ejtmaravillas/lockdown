2ecd4d6a8476147f89d676194f07590c2717cb12
ejtmaravillas <ejtmaravillas@gmail.com>
2020-05-03 06:26:49 +0800

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://ejtmaravillas:Maravillas123!@projectx-l5ix7.mongodb.net/MERN?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://ejtmaravillas:<password>@projectx-l5ix7.mongodb.net/MERN?retryWrites=true&w=majority'
 }
