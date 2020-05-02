1a8fa0e0a9dd8f9c54d05cad4f5ed93a7df9fed8
Eli Joshua <ejtmaravillas@gmail.com>
2020-05-03 06:25:00 +0800

    added user authentication

--- models/Users.js
+++ models/Users.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const moment = require('moment');
+
 const saltRounds = 10;
+
 const Schema = mongoose.Schema;
 
-
+//var userSchema = new Schema({
 var userSchema = new Schema({
     username: {
         type: String,
@@ -11,7 +15,8 @@ var userSchema = new Schema({
     },
     email: {
         type: String,
-        trim: true
+        trim: true,
+        unique: 1
     },
     password: {
         type: String,
@@ -33,27 +38,70 @@ var userSchema = new Schema({
     }
 });
 
-//hash password
-// userSchema.pre('save', ( next ) => {
-//     var user = this;
-//     if(user.isModified('password')) {
-//       bcrypt.genSalt(saltRounds, (err,salt) => {
-        
-//         if(err) return next(err);
-//         bcrypt.hash( user.password, salt, (err,hash) => {
-//             console.log(err);
-//             if(err) return next(err);
-//             user.password = hash;
-//         });
-//       });  
-//     } else {
-        
-//         next();
-//     }
-//     console.log('password is not modified')
-// });
+userSchema.pre('save', function (next) {
+    var user = this;
+    console.log('pre save called');
+    if(user.isModified('password')) {
+        console.log('password changed')
+        bcrypt.genSalt(saltRounds, (err, salt) => {
+            if(err) return next(err);
+            bcrypt.hash(user.password, salt, (err, hash) => {
+                if(err) return next(err);
+                user.password = hash;
+                next();
+            })
+        });
+    } else {
+        next();
+    }
+});
 
+userSchema.methods.comparePassword = function (password, cb) {
+    bcrypt.compare(password, this.password, (err, isMatch) => {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+}
+
+
+userSchema.methods.generateToken = function (cb) {
+    var user = this;
+    console.log(user);
+    var token = jwt.sign({user: user._id.toHexString()},'secret')
+    var oneHour = moment().add(1, 'hour').valueOf();
+
+    user.tokenExp = oneHour;
+    user.token = token;
+    user.save(function (err, user) {
+        if (err) return cb(err);
+        cb(null, user);
+    });
+};
+
+userSchema.methods.generateToken = function (cb) {
+    var user = this;
+    var token = jwt.sign(user._id.toHexString(), 'secret')
+    var oneHour = moment().add(1, 'hour').valueOf();
+
+    user.tokenExp = oneHour;
+    user.token = token;
+    user.save(function (err, user) {
+        if (err) return cb(err)
+        cb(null, user);
+    })
+}
+
+userSchema.statics.fidByToken = function (token, cb) {
+    var user = this;
+
+    jwt.verify(token, 'secret', (err, decode) => {
+        user.findOne({"_id": decode, "token":token }, (err, user) =>{
+            if (err) return cb(err);
+            cb(null, user);
+        })
+    })
+}
 
 const Users = mongoose.model('User', userSchema);
 
-module.exports =  Users;
+module.exports = Users;
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,105 +1,116 @@
-const express = require('express');
+const express = require("express");
 //add router from express
 const router = express.Router();
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 const saltRounds = 10;
 
-const User = require('../../models/Users');
+const User = require("../../models/Users");
 
 //register user
 
-router.get('/', async (req,res) => {
-    try{
-        const users = await User.find();
-        if (!users) throw Error('No Users');
-
-        res.status(200).json(users);
-
-    } catch (e) {
-        res.status(200).json(users);
-    }
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find();
+    if (!users) throw Error("No Users");
+    res.status(200).json(users);
+  } catch (e) {
+    res.status(200).json(users);
+  }
 });
 
+router.post("/sample", (req, res) => {
+    User.findOne({ email: req.body.email }, (err, user) => {
+        if (!user)
+            return res.json({
+                loginSuccess: false,
+                message: "Auth failed, email not found"
+            });
 
-router.post('/register', async (req,res) => {
-    try {
-        const newUser = new User(req.body);
-        CheckUserRegister(newUser, (err2, user) => {
-            if(err2 || !user) {
-                res.status(400).json({ msg: err2});
-            }else{
-                res.status(200).json(user);
-            }
-        });        
-    } catch (e) {
-        res.status(400).json({  msg: e.message});
-    }
-});
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if (!isMatch)
+                return res.json({ loginSuccess: false, message: "Wrong password" });
 
-function CheckUserRegister(user,cb) {
-    User.find({email: user.email}, (err,data) => {
-        console.log(data);
-        if(data.length){
-            cb('Username already exists', null);
-        }else {
-            //hash password from Users model bcrypt userSchema.pre
-            console.log('pre save');
-            //user1 = hashPassword(user);
-            bcrypt.genSalt(saltRounds, (err, salt) => {
-                bcrypt.hash(user.password, salt, (err,hash) => {
-                    user.password = hash;
-                    console.log(user);
-                    user.save((err) => {
-                        cb(err,user);
+            user.generateToken((err, user) => {
+                if (err) return res.status(400).send(err);
+                res.cookie("w_authExp", user.tokenExp);
+                res
+                    .cookie("w_auth", user.token)
+                    .status(200)
+                    .json({
+                        loginSuccess: true, userId: user._id
                     });
-                });
-            });
-        }
-    });
-}
-
-router.post('/login', (req,res) => {
-    try{
-        User.findOne({email: req.body.email}, (err, user) => {
-            const pass = req.body.password;
-            console.log(`input password: ${pass}`);
-            console.log(`user: ${user}`);
-            if(!user){
-                return res.json({
-                    loginSuccess: false,
-                    message: "Auth failed, email not found"
-                });
-            }            
-            //checkLogin = checkPassword(user, pass);
-            bcrypt.compare(pass,user.password, (err, result) => {
-                if(!result) return res.json({ loginSuccess: false, message: "Wrong password" });
-                return res.json({ loginSuccess: true, message: "Login success"});
             });
         });
-    }catch(e) {
-        res.status(400).json({  msg: e.message});
-    }
+    });
 });
 
-function checkPassword(user, password){
-    hash = user.password;
-    bcrypt.compare(password,hash, (err, res) => {
-        return res;
+router.post("/register", async (req, res) => {
+  try {
+    console.log('register start')
+    const newUser = new User(req.body);
+    console.log('register start a')
+    CheckUserRegister(newUser, (err2, user) => {
+      if (err2 || !user) {
+        res.status(400).json({ registerSuccess: false, msg: err2 });
+      } else {
+        res.status(200).json({ registerSuccess: true, msg: user });
+      }
     });
+  } catch (e) {
+    console.log('register error')
+    res.status(400).json({ registerSuccess: false, msg: e.message });
+  }
+});
+
+function CheckUserRegister(user, cb) {
+  User.find({ email: user.email }, (err, data) => {
+    console.log(data);
+    if (data.length) {
+      cb("Username already exists", null);
+    } else {
+      //hash password from Users model bcrypt userSchema.pre
+      console.log("bcrypt save");
+      user.save((err) => {
+        console.log("user registered");
+        cb(err, user);
+      });
+    }
+  });
 }
 
-function hashPassword(user){
-<<<<<<< HEAD
-=======
-    const userC = new User(user)
->>>>>>> 4b5300a791d0600e40cbf30f30d79e530176ea97
-    bcrypt.genSalt(saltRounds, (err, salt) => {
-        bcrypt.hash(user.password, salt, (err,hash) => {
-            userC.password = hash;
+router.post("/login", (req, res) => {
+  try {
+      console.log(User)
+    User.findOne({ email: req.body.email }, (err, user) => {
+      const pass = req.body.password;
+      console.log(user);
+      if (!user) {
+        return res.json({
+          loginSuccess: false,
+          message: "Auth failed, email not found",
         });
+      }
+      user.comparePassword(pass, (err, isMatch) => {
+        if (!isMatch) {
+          return res.json({ loginSuccess: false, message: "Wrong Password" });
+        } else {
+            console.log('login password confirmed')
+        }
+      });
+
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+        res.cookie("x_authExp", user.tokenExp);
+        res.cookie("x_auth", user.token).status(200).json({
+          loginSuccess: true,
+          message: "log in success",
+        });
+        console.log('token generated');
+      });
     });
-    console.log(`UserC: ${userC}`);
-    return userC;
-}
+  } catch (e) {
+    res.status(400).json({ msg: e.message });
+  }
+});
 
 module.exports = router;
